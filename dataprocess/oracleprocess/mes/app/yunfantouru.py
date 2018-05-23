cc9b4b623eebf50e91bc7c2661a34728afbcdb5e
Feng Guoming <fengguoming@cloudin.ren>
2018-05-23 18:40:42 +0800

    Update yunfantouru.py

--- dataprocess/oracleprocess/mes/app/yunfantouru.py
+++ dataprocess/oracleprocess/mes/app/yunfantouru.py
@@ -1,25 +1,15 @@
 from common.DbCommon import oracle2pd,mysql2pd
-
-ora=oracle2pd('10.232.101.51','1521','MESDB','BDATA','BDATA')
-sql=open('dataprocess/oracleprocess/mes/sqls/原反投入.sql','r').read()
-sqlc='''CREATE TABLE `yuanfantouru` (
-`tid` int(8) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-`uptime` datetime(6) COMMENT '更新时间',
-`matno` varchar(20) COMMENT '料号',
-`lot` varchar(20) COMMENT '批号 ',`num` int(6) COMMENT '数量',
-`formno` varchar(20) COMMENT '工单 ',
-PRIMARY KEY (`tid`)
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARSET = utf8;
-'''
-# sqld='drop table yuanfantouru'
-sqlindex='''
-create index timeindex on yuanfantouru(uptime)
-'''
-ms=mysql2pd('123.59.26.236','33333','mysql','root','Rtsecret')
-# ms.dopost(sqld)
-# ms.dopost(sqlc)
-res=ora.doget(sql)
-res.columns=['matno','lot','num','uptime','formno']
-ms.write2mysql(res,'yuanfantouru')
-# print(ms.dopost(sqlindex))
-# ms.getdata('trace_production').to_csv('res.csv')
+class YunFan(object):
+    def __init__(self):
+        super(YunFan, self).__init__()
+    def __del__(self):
+        self.ora.close()
+        self.ms.close()
+    def __call__(self):
+        ora = oracle2pd('10.232.101.51', '1521', 'MESDB', 'BDATA', 'BDATA')
+        sql = open('../sqls/原反投入.sql', 'r').read()
+        ms = mysql2pd('123.59.214.229', '33333', 'offline', 'root', 'Rtsecret')
+        res = ora.doget(sql)
+        res.columns = ['matno', 'lot', 'num', 'uptime', 'formno']
+        ms.dopost('truncate table yuanfantouru')
+        ms.write2mysql(res, 'yuanfantouru')
