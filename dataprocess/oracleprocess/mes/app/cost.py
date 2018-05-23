95682f00202648ccd5078e821d41f3b8f57eeb3f
Feng Guoming <fengguoming@cloudin.ren>
2018-05-23 18:37:08 +0800

    Upload new file 损耗业务程序

---
+++ dataprocess/oracleprocess/mes/app/cost.py
@@ -0,0 +1,24 @@
+from common.DbCommon import mysql2pd,oracle2pd
+
+class Cost(object):
+    def __init__(self):
+        super(Cost, self).__init__()
+
+    def __call__(self):
+        self.ora=oracle2pd('10.232.101.51','1521','MESDB','BDATA','BDATA')
+        self.ms=mysql2pd('123.59.214.229','33333','offline','root','Rtsecret')
+        with open('../sqls/损耗.sql','r') as f:
+            sql=f.read()
+        res = self.ora.doget(sql)
+        print(res[:10])
+        self.ms.dopost('truncate table sunhao')
+        print(res.shape)
+        total=res.shape[0]
+        nowrow=0
+        while nowrow<total-1000:
+            print(self.ms.write2mysql(res[nowrow:nowrow+1000], 'sunhao'))
+            nowrow+=1000
+        self.ms.write2mysql(res[nowrow:], 'sunhao')
+    def __del__(self):
+        self.ora.close()
+        self.ms.close()
