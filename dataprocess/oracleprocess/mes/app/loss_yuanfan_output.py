11ec2d5dc599ca64246dec3b326bef36e9f2212e
Feng Guoming <fengguoming@cloudin.ren>
2018-07-02 12:09:34 +0800

    原反产出损耗程序修改

--- dataprocess/oracleprocess/mes/app/loss_yuanfan_output.py
+++ dataprocess/oracleprocess/mes/app/loss_yuanfan_output.py
@@ -1,5 +1,4 @@
 from dataprocess.oracleprocess.mes.base import Base
-import datetime
 class LossYuanfanOutput(object):
     def __inint__(self):
         super(LossYuanfanOutput,self).__init__()
@@ -9,11 +8,9 @@ class LossYuanfanOutput(object):
         offline=conns['offline']
         with open(base.path1+'sqls/原返损耗产出.sql','r') as f:
             sql=f.read()
-        #offline.dopost("truncate table loss_yuanfan_output")
-        #for d in range(1,27):
-        yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-        #yesterday=datetime.datetime(2018,6,d)
-        date = datetime.datetime.strftime(yesterday,'%Y/%m/%d')
-        res = mes.doget(sql.replace('thistime', date))
-        res['業務日期']=date
-        base.batchwri(res, 'loss_yuanfan_output', offline)
+        offline.dopost("truncate table loss_yuanfan_output")
+        for date in [base.gettoday(),base.getYesterday()]:
+            res = mes.doget(sql.replace('thistime', date))
+            res['業務日期']=date
+            offline.dopost("delete from loss_yuanfan_output where 業務日期='"+date+"'")
+            base.batchwri(res, 'loss_yuanfan_output', offline)
